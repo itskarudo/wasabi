08f8ffc06dc553b2fe45e9779677f9f5f89d6070
itskarudo <itskarudo@protonmail.com>
2022-09-05 21:28:43 +0100

    Awesome: Add fullscreen toggle shortcut

--- awesome/.config/awesome/keys.lua
+++ awesome/.config/awesome/keys.lua
@@ -48,6 +48,7 @@ keys.globalkeys = gears.table.join(
 -- Keyboard Control
 keys.clientkeys = gears.table.join(
   awful.key({mod}, 'w', function(c) c:kill() end),
+  awful.key({mod}, 'f', function(c) c.fullscreen = not c.fullscreen; c:raise() end),
   awful.key({mod}, 'Tab', function() awful.client.floating.toggle() end)
 )
 
